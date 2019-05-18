84c2b10f505120b6ad2bc4e5a25761d771c551e1
Alexandra Dima <alexandra.dima89@yahoo.com>
2019-05-18 18:58:50 +0200

    Cart

---
+++ RentingWebsite/ViewModels/CartIndexListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RentingWebsite.Models;
+using RentingWebsite.ViewModels;
+
+namespace RentingWebsite.ViewModels
+{
+    public class CartIndexViewModel
+    {
+        //properties 
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ RentingWebsite/ViewModels/CartLines.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RentingWebsite.Models;
+using RentingWebsite.ViewModels;
+
+namespace RentingWebsite.ViewModels
+{
+    public class CartLines
+    {
+        /// to represent a product selected by the customer and the quantity the user wants to buy
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        ///end public class CartLine
+        ///
+    }
+}
