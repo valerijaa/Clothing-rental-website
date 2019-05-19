0f3243d1163773bd050aa67096d0cf727291ed54
valerijaaa <36069107+valerijaaa@users.noreply.github.com>
2019-05-19 19:45:25 +0200

    filtering functionality (no styling yet)

---
+++ RentingWebsite/ViewModels/FilterForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentingWebsite.ViewModels
+{
+    public class FilterForm
+    {
+        public List<FilterItem> Types { get; set; }
+        public List<FilterItem> Sizes { get; set; }
+        public List<FilterItem> Colors { get; set; }
+        public List<FilterItem> Fits { get; set; }
+    }
+}
