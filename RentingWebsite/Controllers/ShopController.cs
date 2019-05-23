cc9ac5d5708d416d72bf343220e863be6c04f459
maciekwoz <33222646+dobish@users.noreply.github.com>
2019-05-23 10:12:56 +0200

    shop_controller

--- RentingWebsite/Controllers/ShopController.cs
+++ RentingWebsite/Controllers/ShopController.cs
@@ -41,56 +41,35 @@ namespace RentingWebsite.Controllers
         }
 
         //View all products
-        public ActionResult Index(/*string Category*/)
-        {
-            var products = db.Products.Include(p => p.FitProduct)
-                                    .Include(p => p.Image);
-            return View(products.ToList());
-        }
-
-        //Order by Category
-        public ActionResult Category(string Category)
+        public ActionResult Index()
         {
             var products = db.Products.Include(p => p.FitProduct)
                                       .Include(p => p.Image);
-            products = db.Products
-               //Update the Catalog so that the Index action will filter the Product objects
-               .Where(p => p.Category == null || p.Category == p.Category)  //If category is not null, only those Product objects with a matching Category property are selected
-               .OrderBy(p => p.ProductId); //Get the product objects and order them by ProductId
-       
-                        //.Skip((page - 1) * PageSize) //Skip over the products thta occur before the start of the current page 
-                        //.Take(PageSize); //Take the number of products specified by the PageSize field
-                        //                 //Adding the PagingInfo method to diplay the details of the pagination
-                        //PagingInfo = new PagingInfo
-                        //{
-                        //    CurrentPage = page,
-                        //    ItemsPerPage = PageSize,
-                        //    TotalItems = Category == null ?
-                        //    db.Products.Count() :
-                        //    db.Products.Where(e => e.Category == Category).Count() //the pagination information takes the categories into account
-                        //};
-                       var CurrentCategory = Category;
 
-
-            //These changes pass a ProductsListViewModel object as the model data to the view
             return View(products.ToList());
-
         }
-        //Order by Category
-        public ActionResult Price()
+
+
+
+
+        public ActionResult ProductDetails(int? id)
         {
             RentingWebsiteEntities db = new RentingWebsiteEntities();
-            var products = db.Products.Include(p => p.FitProduct)
-                                      .Include(p => p.Image);
-            products = db.Products
-                   //Update the Catalog so that the Index action will filter the Product objects
-                   .Where(p => p.Price == null || p.Price == p.Price)  //If category is not null, only those Product objects with a matching Category property are selected
-                      .OrderBy(p => p.ProductId);
 
-            return View();
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+
+            return View(product);
         }
 
-        
+
+
+
+
 
 
     }
