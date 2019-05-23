da45dd6a5faa439218636ed9469d04ff35a53600
valerijaaa <36069107+valerijaaa@users.noreply.github.com>
2019-05-23 19:36:58 +0200

    Bundle config ignore obsolete stylesheet

--- RentingWebsite/App_Start/BundleConfig.cs
+++ RentingWebsite/App_Start/BundleConfig.cs
@@ -26,8 +26,7 @@ namespace RentingWebsite
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css"));
         }
     }
 }
